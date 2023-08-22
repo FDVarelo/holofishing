533fe5726448fe8c4b3b97d5f390431475f687fc
Fred Daniel Varelo <43612823+FDVarelo@users.noreply.github.com>
2023-08-22 18:27:27 -0300

    Update that worked better for me
    - Removed the condition IsFishing do to directly on the if result
    - Removed constants from the loop
    - mss.mss() before the while to do only one time

--- holofishingbot.py
+++ holofishingbot.py
@@ -1,49 +1,49 @@
-import mss.tools
+import mss
 import cv2 as cv
 import numpy as np
 import pydirectinput as di
 
-INDICATOR = cv.imread('assets/indicator.png', cv.IMREAD_GRAYSCALE)
+# Images models
+INDICATOR = cv.imread('assets/indicator.png', cv.IMREAD_GRAYSCALE) # Fishing model
 UP = cv.imread('assets/up.png', cv.IMREAD_GRAYSCALE)
 DOWN = cv.imread('assets/down.png', cv.IMREAD_GRAYSCALE)
 LEFT = cv.imread('assets/left.png', cv.IMREAD_GRAYSCALE)
 RIGHT = cv.imread('assets/right.png', cv.IMREAD_GRAYSCALE)
 CIRCLE = cv.imread('assets/circle.png', cv.IMREAD_GRAYSCALE)
 
-isFishing = False
+isFishingLocal = {"top": 270, "left": 1224, "width": 51, "height": 81} # Fishing mode location
+captureMovNow = {"top": 708, "left": 1131, "width": 96, "height": 96}  # Local to capture the input to do now
 
-while (True):
-    with mss.mss() as sct:
-        indicatorDim = {"top": 270, "left": 1224, "width": 51, "height": 81}
-        indicator = np.array(sct.grab(indicatorDim))
-        targetDim = {"top": 708, "left": 1131, "width": 96, "height": 96}
-        target = np.array(sct.grab(targetDim))
+with mss.mss() as sct:
+    while True:
+        # Verifying if is now on fishing mode
+        Fishing = np.array(sct.grab(isFishingLocal))
+        Fishing = cv.cvtColor(Fishing, cv.COLOR_BGRA2GRAY)
+        resultFishing = cv.matchTemplate(Fishing,INDICATOR,cv.TM_CCOEFF_NORMED)
 
-    indicator = cv.cvtColor(indicator, cv.COLOR_BGRA2GRAY)
-    resIndicator = cv.matchTemplate(indicator, INDICATOR, cv.TM_CCOEFF_NORMED)
+        # If is on fishing mode
+        if(cv.minMaxLoc(resultFishing)[1] > 0.9):
+            
+                MovNow = np.array(sct.grab(captureMovNow))
+                target = cv.cvtColor(MovNow, cv.COLOR_BGRA2GRAY)
 
-    if (cv.minMaxLoc(resIndicator)[1] > 0.9):
-        isFishing = True
-    else:
-        isFishing = False
-        di.press("e")
+                resUp = cv.matchTemplate(target, UP, cv.TM_CCOEFF_NORMED)
+                resDown = cv.matchTemplate(target, DOWN, cv.TM_CCOEFF_NORMED)
+                resLeft = cv.matchTemplate(target, LEFT, cv.TM_CCOEFF_NORMED)
+                resRight = cv.matchTemplate(target, RIGHT, cv.TM_CCOEFF_NORMED)
+                resCircle = cv.matchTemplate(target, CIRCLE, cv.TM_CCOEFF_NORMED)
 
-    if isFishing:
-        target = cv.cvtColor(target, cv.COLOR_BGRA2GRAY)
+                if (cv.minMaxLoc(resUp)[1] >= 0.9):
+                    di.press("w")
+                elif (cv.minMaxLoc(resDown)[1] >= 0.9):
+                    di.press("s")
+                elif (cv.minMaxLoc(resLeft)[1] >= 0.9):
+                    di.press("a")
+                elif (cv.minMaxLoc(resRight)[1] >= 0.9):
+                    di.press("d")
+                elif (cv.minMaxLoc(resCircle)[1] >= 0.9):
+                    di.press("space")
 
-        resUp = cv.matchTemplate(target, UP, cv.TM_CCOEFF_NORMED)
-        resDown = cv.matchTemplate(target, DOWN, cv.TM_CCOEFF_NORMED)
-        resLeft = cv.matchTemplate(target, LEFT, cv.TM_CCOEFF_NORMED)
-        resRight = cv.matchTemplate(target, RIGHT, cv.TM_CCOEFF_NORMED)
-        resCircle = cv.matchTemplate(target, CIRCLE, cv.TM_CCOEFF_NORMED)
-
-        if (cv.minMaxLoc(resUp)[1] > 0.9):
-            di.press("w")
-        elif (cv.minMaxLoc(resDown)[1] > 0.9):
-            di.press("s")
-        elif (cv.minMaxLoc(resLeft)[1] > 0.9):
-            di.press("a")
-        elif (cv.minMaxLoc(resRight)[1] > 0.9):
-            di.press("d")
-        elif (cv.minMaxLoc(resCircle)[1] > 0.9):
-            di.press("e")
+        # Not on fishing mode
+        else:
+            di.press("space")
