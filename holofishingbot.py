e7b43c251be14ffb290b88514d2ea0738f932e99
Fred Daniel Varelo <43612823+FDVarelo@users.noreply.github.com>
2023-08-23 20:53:35 -0300

    New way to get if is fishing now

--- holofishingbot.py
+++ holofishingbot.py
@@ -4,27 +4,28 @@ import numpy as np
 import pydirectinput as di
 import keyboard
 
-# Images models
-INDICATOR = cv.imread('assets/indicator.png', cv.IMREAD_GRAYSCALE) # Fishing model
+### Images models.
+INDICATOR = cv.imread('assets/indicator.png', cv.IMREAD_GRAYSCALE) ### Fishing model.
 UP = cv.imread('assets/up.png', cv.IMREAD_GRAYSCALE)
 DOWN = cv.imread('assets/down.png', cv.IMREAD_GRAYSCALE)
 LEFT = cv.imread('assets/left.png', cv.IMREAD_GRAYSCALE)
 RIGHT = cv.imread('assets/right.png', cv.IMREAD_GRAYSCALE)
 CIRCLE = cv.imread('assets/circle.png', cv.IMREAD_GRAYSCALE)
 
-isFishingLocal = {"top": 270, "left": 1224, "width": 51, "height": 81} # Fishing mode location
-captureMovNow = {"top": 708, "left": 1121, "width": 106, "height": 96}  # Local to capture the input to do now
+isFishingLocal = {"top": 270, "left": 1224, "width": 51, "height": 81} ### Fishing mode location.
+captureMovNow = {"top": 708, "left": 1121, "width": 106, "height": 96} ### Local to capture the input to do now.
 
 with mss.mss() as sct:
-    while keyboard.is_pressed('q') == False: # hold 'q' to quit
-        # Verifying if is now on fishing mode
-        Fishing = np.array(sct.grab(isFishingLocal))
-        Fishing = cv.cvtColor(Fishing, cv.COLOR_BGRA2GRAY)
-        resultFishing = cv.matchTemplate(Fishing,INDICATOR,cv.TM_CCOEFF_NORMED)
+    while keyboard.is_pressed('q') == False: ### hold 'q' to quit.
+        ### Verifying if is now on fishing mode
+        r,g,b = pyautogui.pixel(1181,696) ### Get one pixel of the arrow above the input now, faster than getting the fishing icon.
+        #Fishing = np.array(sct.grab(isFishingLocal))
+        #Fishing = cv.cvtColor(Fishing, cv.COLOR_BGRA2GRAY)
+        #resultFishing = cv.matchTemplate(Fishing,INDICATOR,cv.TM_CCOEFF_NORMED)
 
-        # If is on fishing mode
-        if(cv.minMaxLoc(resultFishing)[1] > 0.9): #since is stating, it can be > 0.9
-            
+        ### If is on fishing mode.
+        #if(cv.minMaxLoc(resultFishing)[1] > 0.9): ###since is stating, it can be > 0.9
+        if r == 251 and g == 251 and b == 251: ### Faster than verifying the minMaxLoc and searching similar images.    
                 MovNow = np.array(sct.grab(captureMovNow))
                 target = cv.cvtColor(MovNow, cv.COLOR_BGRA2GRAY)
 
@@ -34,17 +35,17 @@ with mss.mss() as sct:
                 resRight = cv.matchTemplate(target, RIGHT, cv.TM_CCOEFF_NORMED)
                 resCircle = cv.matchTemplate(target, CIRCLE, cv.TM_CCOEFF_NORMED)
 
-                if (cv.minMaxLoc(resUp)[1] > 0.8):
+                if (cv.minMaxLoc(resUp)[1] > 0.7):
                     di.press("w")
-                elif (cv.minMaxLoc(resDown)[1] > 0.8):
+                elif (cv.minMaxLoc(resDown)[1] > 0.7):
                     di.press("s")
-                elif (cv.minMaxLoc(resLeft)[1] > 0.8):
+                elif (cv.minMaxLoc(resLeft)[1] > 0.7):
                     di.press("a")
-                elif (cv.minMaxLoc(resRight)[1] > 0.8):
+                elif (cv.minMaxLoc(resRight)[1] > 0.7):
                     di.press("d")
-                elif (cv.minMaxLoc(resCircle)[1] > 0.8):
+                elif (cv.minMaxLoc(resCircle)[1] > 0.7):
                     di.press("space")
 
-        # Not on fishing mode
+        ### Not on fishing mode.
         else:
             di.press("space")
